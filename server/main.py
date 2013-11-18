810a289dd80a8bdb8cee99b4dcc46d56b1bbe0da
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-11-18 20:03:16 +0100

    Simple "template" HTML file serving.

--- server/main.py
+++ server/main.py
@@ -4,12 +4,20 @@ from flask_sockets import Sockets
 app = Flask(__name__)
 sockets = Sockets(app)
 
+
+def render_file(filename, *args, **kwargs):
+    s = open(filename).read()
+    # TODO: string/replace in s, cheap .50$ templates.
+    return s
+
+
 @sockets.route('/echo')
 def echo_socket(ws):
     while True:
         message = ws.receive()
         ws.send(message)
 
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_file("template/index.html")
