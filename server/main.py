f34084902c7b365ff7d7242b8ddf2227fea03519
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-11-18 20:57:12 +0100

    Flask templates and running through gevent.

--- server/main.py
+++ server/main.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_sockets import Sockets
 
 app = Flask(__name__)
+app.debug = True
 sockets = Sockets(app)
 
 
 def render_file(filename, *args, **kwargs):
-    s = open(filename).read()
-    # TODO: string/replace in s, cheap .50$ templates.
-    return s
+    return render_template(filename, *args, **kwargs)
 
 
 @sockets.route('/echo')
@@ -18,6 +17,19 @@ def echo_socket(ws):
         ws.send(message)
 
 
+@app.route('/game/<gameid>')
+def game(gameid):
+    return render_file("game.html", url=request.url)
+
+
 @app.route('/')
 def hello():
-    return render_file("template/index.html")
+    return render_file("index.html")
+
+
+if __name__ == '__main__':
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+
+    server = pywsgi.WSGIServer(('', 8000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
