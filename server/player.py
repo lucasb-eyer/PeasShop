82e86e9276e5aafda7ba76b10fa59ba9b77c7a01
Pandoro <alexander.hermans0@gmail.com>
2013-11-19 06:05:04 +0100

    Color bonus

--- server/player.py
+++ server/player.py
@@ -7,6 +7,7 @@ class Player(object):
         self.game = game
         self.item_dict = {item['id']: item for item in game.item_list}
         self.score = 0.0
+        self.bonus = 0.0
     def send(self, msg):
         self.ws.send(json.dumps(msg))
 
@@ -19,15 +20,24 @@ class Player(object):
         if( self.money >= cost):
             if(slot not in self.slots)
                 self.money -= cost
-                self.score += item_dict[item_id]['rating']
+                self.score += self.item_dict[item_id]['rating']
                 self.slots[item_dict[item_id]['type']] = item_dict[item_id]
 
+                bon = len(self.slots) > 1
+                color = item_dict[item_id]['color']
+                for k, v in self.slots :
+                    if v['color'] != color :
+                        bon = False
 
-
-
+                if bon :
+                    self.bonus = 3*len(self.slots)
+                else :
+                    self.bonus = 0
                 self.send({'type' : 'you_took', 'item' : item_id, 'money' :
-                           self.money, 'score' : self.score})
+                           self.money, 'score' : self.score},
+                          'color_bonus' : self.bonus)
                 self.game.opponent(self).send({'type' : 'other_took', 'item' :
                                                item_id, 'other_money' :
                                                self.money, 'other_score' :
-                                               self.score})
+                                               self.score,
+                                               'other_color_bonus' : self.bonus)
